50ad18126bb3d7ac1f1828fbe4255d5083f76715
sachitpandey <57258620+sachitpandey@users.noreply.github.com>
2021-04-30 15:44:29 +0530

    db change
    changed from sqllite to postgres

--- crud/settings.py
+++ crud/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Artdb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
